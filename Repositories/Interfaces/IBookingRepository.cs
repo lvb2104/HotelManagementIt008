3095224161c6eeb973a1cfd991cafa488915ef64
minh10m <minhhh1010@gmail.com>
2025-11-26 09:48:04 +0700

    feat(booking): edit repo

--- Repositories/Interfaces/IBookingRepository.cs
+++ Repositories/Interfaces/IBookingRepository.cs
@@ -3,5 +3,7 @@
     // Define methods specific to Booking entity
     public interface IBookingRepository : IRepository<Booking>
     {
+        Task<IEnumerable<Booking>> FindOverlappingBookingsAsync(Guid roomId, DateTime checkIn, DateTime checkOut, Guid? excludeBookingId = null);
+        Task<IEnumerable<Booking>> FindUserOverlappingBookingsAsync(Guid userId, DateTime checkIn, DateTime checkOut, Guid? excludeBookingId = null);
     }
 }
