2d7946d51e59a0410fc18beddfafd3842da85dee
minh10m <minhhh1010@gmail.com>
2025-11-26 09:49:40 +0700

    feat(invoice): edit repo

--- Repositories/Implementations/InvoiceRepository.cs
+++ Repositories/Implementations/InvoiceRepository.cs
@@ -1,10 +1,40 @@
-﻿namespace HotelManagementIt008.Repositories.Implementations
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HotelManagementIt008.Repositories.Implementations
 {
     public class InvoiceRepository : Repository<Invoice>, IInvoiceRepository
     {
         public InvoiceRepository(HotelManagementDbContext context) : base(context)
         {
 
+        }
+
+        public async Task<IEnumerable<Invoice>> GetInvoicesByMonthOrYearAsync(int? year, int? month)
+        {
+            if (year == null && month == null)
+            {
+                return await GetAllQueryable().OrderBy(i => i.UpdatedAt).ToListAsync();
+            }
+
+            var query = GetAllQueryable();
+
+            if (year.HasValue)
+            {
+                query = query.Where(i => i.UpdatedAt.Year == year.Value);
+            }
+
+            if (month.HasValue)
+            {
+                query = query.Where(i => i.UpdatedAt.Month == month.Value);
+
+                if (!year.HasValue)
+                {
+                    var currentYear = DateTime.Now.Year;
+                    query = query.Where(i => i.UpdatedAt.Year == currentYear);
+                }
+            }
+
+            return await query.OrderBy(i => i.UpdatedAt).ToListAsync();
         }
     }
 }
