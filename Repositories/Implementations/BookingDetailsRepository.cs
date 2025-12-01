03d713af6554b0bf1c52916b0864638d1fdd2bc8
minh10m <minhhh1010@gmail.com>
2025-12-01 09:55:11 +0700

    feat(booking detai): add function

--- Repositories/Implementations/BookingDetailsRepository.cs
+++ Repositories/Implementations/BookingDetailsRepository.cs
@@ -1,10 +1,22 @@
-﻿namespace HotelManagementIt008.Repositories.Implementations
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HotelManagementIt008.Repositories.Implementations
 {
     public class BookingDetailsRepository : Repository<BookingDetails>, IBookingDetailsRepository
     {
         public BookingDetailsRepository(HotelManagementDbContext context) : base(context)
         {
+        }
 
+        public async Task<IEnumerable<BookingDetails>> GetByBookingIdAsync(Guid bookingId)
+        {
+            return await GetAllQueryable()
+                .Include(bd => bd.User)
+                    .ThenInclude(u => u.Profile)
+                .Include(bd => bd.User)
+                    .ThenInclude(u => u.UserType)
+                .Where(bd => bd.BookingId == bookingId)
+                .ToListAsync();
         }
     }
 }
